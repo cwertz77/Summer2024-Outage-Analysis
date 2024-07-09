55e9d3c5d5bf890df2443093ebe367e1abb7679c
Irishumanoid <95321751+Irishumanoid@users.noreply.github.com>
2024-07-09 15:30:00 -0700

    filtered datasets

--- EIA_data_filterer.py
+++ EIA_data_filterer.py
@@ -1,35 +1,49 @@
+from enum import Enum
 import pandas as pd
 
+class sheet_type(Enum):
+    ADV_METERS = 1,
+    BAL_AUTH = 2,
+    DEMAND_RESP = 3,
+    DIST_SYST = 4,
+    DYNAMIC_PRICING = 5,
+    ENERGY_EFF = 6,
+    FRAME = 7,
+    MERGERS = 8,
+    NET_METERING = 9,
+    NON_NET_METERING = 10,
+    OP_DATA = 11,
+    RELIABILITY = 12,
+    SALES_ULT_CUST = 13,
+    SALES_ULT_CUST_CS = 14,
+    SERV_TERR = 15,
+    SHORT_FORM = 16,
+    UTIL_DATA = 17
 
-#Advanced Meters
-index_to_desc = {
-    '0': 'Number AMR-Automated Meter Reading',
-    '1': 'Number AMI-Advanced Metering Infrastructure',
-    '2': 'Number Home Area Network',
-    '3': 'Number Standard (non AMR/AMI) Meters',
-    '4': 'Total Numbers of Meters',
-    '5': 'Energy Served - AMI (MWh)',
-    '6': 'Customers with Daily Digital Access',
-    '7': 'Customers with Direct Load Control'
-}
-
-def filter_csv(csv_path):
+def filter_csv(csv_path, type: sheet_type):
     df = pd.read_csv(csv_path)
     illinois_df = pd.DataFrame()
+    key = ''
+
+    if type in [sheet_type.DEMAND_RESP, sheet_type.DYNAMIC_PRICING, sheet_type.ENERGY_EFF, sheet_type.NET_METERING, sheet_type.OP_DATA, sheet_type.RELIABILITY, sheet_type.SALES_ULT_CUST]:
+        key = '|Utility Characteristics..State|'
+    elif type == sheet_type.NON_NET_METERING or sheet_type.UTIL_DATA:
+        key = '|Utility Characteristics.State|'
+    elif type == sheet_type.SALES_ULT_CUST_CS:
+        key = '|Utility Characteristics..STATE|'
+    elif type == sheet_type.SERV_TERR or sheet_type.SHORT_FORM:
+        key = '|State|'
+    elif type == sheet_type.FRAME:
+        key = 'Utility Name'
+    else:
+        key = 'State'
+
     for i in range(len(df)):
-        if df.iloc[i]['State'] == 'IL':
+        if df.iloc[i][key] == 'IL' or (type == sheet_type.FRAME and (str(df.iloc[i][key]).__contains__('IL') or str(df.iloc[i][key]).__contains__('Illinois'))):
             copy = df.iloc[i]
             copied_row_df = pd.DataFrame([copy])
             illinois_df = pd.concat([illinois_df, copied_row_df], ignore_index=True) #reindex dataset
-    for col in illinois_df.columns:
-        for i in range(8):
-            if col.__contains__(str(i)):
-                illinois_df = illinois_df.rename(columns={col: '|' + col.replace(str(i), index_to_desc[str(i)]) + '|'})
 
-    print(illinois_df)
     save_path = csv_path[:-4] + '_Illinois.csv'
     with open(save_path, 'w') as f:
-        f.write(illinois_df.to_string())
-
-for year in range(2014, 2023):
-    filter_csv(f'/Users/irislitiu/work/WSU_Outage_Analysis/energy_information_administration_data/{year}/Advanced_Meters_{year}.csv')
+        f.write(illinois_df.to_csv())
---
+++ csv_utils.py
@@ -0,0 +1,59 @@
+import pandas as pd
+
+def convert_xlsx_to_csv(excel_path, csv_save_path, check=False):
+    try:
+        pd.read_excel(excel_path).to_csv(csv_save_path, index=None, header=True)
+        if check:
+            dataframe = pd.DataFrame(pd.read_csv(csv_save_path))
+            dataframe
+    except IOError as e:
+        print(f"file type conversion unsupported: {e}")
+
+def is_not_name_column(column: str, year):
+    return not (column.__contains__(year) or column.__contains__('O = Observed') or column.__contains__('I = Imputed'))
+
+def normalize_columns(csv_path):
+    year = csv_path.split('/')[-2]
+    lines = []
+    with open(csv_path, 'r+') as f:
+        lines = f.readlines()
+        list_of_cols = list(list())
+        index = 0
+        #iterate through rows until hit row with number
+        while is_not_name_column(lines[index], year):
+            #populate all so none remain empty, then move lowest ones up
+            out = lines[index].split(',')
+            cur_actual_entry = ''
+
+            for i in range(len(out)):
+                if out[i] != '':
+                    cur_actual_entry = out[i]
+                else:
+                    out[i] = cur_actual_entry
+
+            if i != len(out)-1:
+                out[i] += ','
+
+            list_of_cols.append(out)
+            index += 1
+        
+        #back populate upper rows until all full
+        for i in range(len(list_of_cols)):
+            cur_index = len(list_of_cols) - i - 1
+            if cur_index - 1 >= 0:
+                for j in range(len(list_of_cols[cur_index])):
+                    #pass info back into previous row
+                    list_of_cols[cur_index - 1][j] += '.' + list_of_cols[cur_index][j]
+
+    for i in range(len(list_of_cols)):
+        lines[i] = ','.join(["|" + val + "|" for val in list_of_cols[i]])
+    #only care about the first entry in the list_of_cols array; delete the rest from the csv
+    indices_to_remove = [i for i in range(1, len(list_of_cols))] 
+    new_lines = []
+    for i in range(len(lines)):
+        if i not in indices_to_remove:
+            new_lines.append(lines[i])
+
+    with open(csv_path, 'w+') as f:
+        f.writelines([line for line in new_lines])
+
